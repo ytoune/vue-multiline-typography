7d2ee8252651a6897e6a44019465c530ee6fa59f
toune <ytoune@users.noreply.github.com>
2020-11-26 02:36:22 +0900

    mod

--- scripts/release.ts
+++ scripts/release.ts
@@ -98,12 +98,12 @@ const publishDir = async ({ resolve, outdir, version }: Context) => {
 				message: `Release v${version}!!`,
 				// tag: `v${version}`,
 			},
-			(e) => (e ? j(e) : r()),
+			e => (e ? j(e) : r()),
 		),
 	)
 }
 
-main().catch((x) => {
+main().catch(x => {
 	console.error(x)
 	process.exit(1)
 })
